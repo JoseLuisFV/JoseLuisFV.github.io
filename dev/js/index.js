b355cdd7373f6d6e87ed8886028ed22d1e5084ee
Jose Flores Vacio <josefvacio@gmail.com>
2021-08-31 13:20:52 -0700

    Carousel de proyectos terminado, pero le quite los proyectos para ver mas a detalle el funcionamiento de carousel

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,6 +1,12 @@
 import { projects } from "./projects.js";
 
+const stackProjects = [];
 
-projects.forEach(project => {
+projects.forEach((project, count = 2, stack = {}) => {
     console.log(project.title);
+    if (count % 3 === 0) {
+        console.log(`${count} / 3 no hay residuo`);
+    } else {
+        console.log(`${count} / 3 residuo = ${count % 3}`);
+    }
 })
--- dev/js/projects.js
+++ dev/js/projects.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         "title": "Stats card Component",
-        "description": "This work is a challenge from Frontend Mentor, in this challege i create a card component is resposive",
+        "description": "This work is a challenge from Frontend Mentor, in this challege i create a card component is resposive , i implement mobilefirst, BEM and Flexbox",
         "icons" : {
             "html": "../assets/icons/icon-html.png",
             "css": "../assets/icons/icon-css.png",
@@ -9,9 +9,10 @@ const projects = [
         "img": "https://i.ibb.co/d03TH6W/desktop-design.jpg" 
     },
 
+    
     {
-        "title": "Stats card Component",
-        "description": "This work is a challenge from Frontend Mentor, in this challege i create a card component is resposive",
+        "title": "3 Column Card",
+        "description": "This work is a challenge from Frontend Mentor, in this challege i create a card that contains 3 kind of car in rent with their description, i implement mobilefirst, BEM and Flexbox",
         "icons" : {
             "html": "../assets/icons/icon-html.png",
             "css": "../assets/icons/icon-css.png",
@@ -20,14 +21,39 @@ const projects = [
     },
 
     {
-        "title": "Stats card Component",
-        "description": "This work is a challenge from Frontend Mentor, in this challege i create a card component is resposive",
+        "title": "FAQ accordion card",
+        "description": "This work is a challenge from Frontend Mentor, in this challege i create a FAQ accordion, it's responsive, when you click on a question our answear is oponed, i implement mobilefirst, BEM and Flexbox",
+        "icons" : {
+            "html": "../assets/icons/icon-html.png",
+            "css": "../assets/icons/icon-css.png",
+            "js": "../assets/icons/icon-js.png"
+        },
+        "img": "https://i.ibb.co/d03TH6W/desktop-design.jpg"
+    },
+
+    {
+        "title": "Social Proof Section",
+        "description": "This work is a challenge from Frontend Mentor, in i create a section that contains a social proof about product's quality, it is responsive, i implemnt mobilefirst, Flexbox, BEM and some transitions on desktop view",
         "icons" : {
             "html": "../assets/icons/icon-html.png",
             "css": "../assets/icons/icon-css.png",
         },
         "img": "https://i.ibb.co/d03TH6W/desktop-design.jpg"
-    }
+    },
+
+    {
+        "title": "Article preview component",
+        "description": "This work is a challenge from Frontend Mentor, in i create a card component that it's a preview about an article, it is responsive, i implemnt mobilefirst, Flexbox, BEM and some transitions",
+        "icons" : {
+            "html": "../assets/icons/icon-html.png",
+            "css": "../assets/icons/icon-css.png",
+            "js": "../assets/icons/icon-js.png",
+        },
+        "img": "https://i.ibb.co/d03TH6W/desktop-design.jpg"
+    },
+
+
+
 ]
 
 export { projects };

899a0887dea06e77576d8c69fe1b354646664b95
Jose Flores Vacio <josefvacio@gmail.com>
2021-09-01 20:43:18 -0700

    Se hizo el carousel funcional y ademas se estiliso el contenido de los slider del carousel, aun falta detallar estilos de los proyectos

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,5 +1,29 @@
 import { projects } from "./projects.js";
 
+// Carousel logic
+
+const mover = document.getElementById('CarouselViewport');
+const anchoSlide = document.querySelector('.carousel__slide').clientWidth;
+
+const buttonPrev = document.getElementById('BotonPrev');
+const buttonNext = document.getElementById('BotonNext');
+
+const toNext = () => {
+  mover.scroll(mover.scrollLeft + anchoSlide, 0);
+}
+
+const toPrev = () => {
+  mover.scroll(mover.scrollLeft - anchoSlide, 0);
+}
+
+buttonNext.addEventListener('click', () => {
+    toNext();
+});
+
+buttonPrev.addEventListener('click', () => {
+    toPrev();
+})
+
 const stackProjects = [];
 
 projects.forEach((project, count = 2, stack = {}) => {
